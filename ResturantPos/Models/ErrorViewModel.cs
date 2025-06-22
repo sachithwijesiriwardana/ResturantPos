c7cad263d5ca58608dc51a28b41f78146bd7c3e4
Sachith Wijeisriwardana <sachith55cha1w@gmail.com>
2025-06-22 22:56:53 +0530

    Add project files.

---
+++ ResturantPos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ResturantPos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
